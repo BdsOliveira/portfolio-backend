4b4e9f778f6db9ab20ccea418e57b8a4796de525
Bruno Oliveira <bds.commus@gmail.com>
2024-09-03 14:34:32 -0300

    fix PORT

--- src/app.js
+++ src/app.js
@@ -12,13 +12,14 @@ app.use(express.json());
 registerRoutes(app)
 
 // Sensible data 
+const PORT = process.env.PORT || 3000;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
  // Setting a port Server
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is starting at port: ${process.env.PORT || 3000}`);
+app.listen(PORT, () => {
+    console.log(`Server is starting at port: ${PORT}`);
 })
 // // Connecting to database
 // mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projects-backend.8algy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
